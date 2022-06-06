d2b2f9a95f14e6b466a4f5da6811eeb25ef93c48
Penghui Guo <m@guo.ph>
2022-06-06 22:44:15 +0800

    structuring project directory & release on PyPI

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name="PyVRP",
+    version="0.0.1",
+    description="A Python package for plotting Vehicle Routing Problem (VRP) solutions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="GPL-3.0",
+    url="https://guo.ph/pyvrp",
+    
+    author="Penghui Guo",
+    author_email="m@guo.ph",
+    
+    packages=["pyvrp"],
+    install_requires=[]
+)
